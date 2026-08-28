6930d0a0e71dcce5ea4bae73ce1dcfa08d9dcdb7
EDWARDAV0228 <akmalosh8484@gmail.com>
2026-08-28 10:06:39 +0500

    Admin custom 2

--- products_app/admin.py
+++ products_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from products_app.models import Products, Category, Brand
 
 
@@ -6,11 +7,28 @@ from products_app.models import Products, Category, Brand
 
 @admin.register(Products)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'brand', 'category', 'date', 'ubdate_date',)
+    list_display = ('id', 'name', 'price', 'brand', 'category', 'date', 'ubdate_date',)
     list_display_links = ('name',)
     list_filter = ('category', 'brand',)
     readonly_fields = ('date', 'ubdate_date')
 
 
-admin.site.register(Category)
-admin.site.register(Brand)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    
+    def display_image(self, obj):
+        if obj and obj.image:
+            return mark_safe(f'<img src"{obj.image.url}" width="100px">')
+        return 'Нет изображения'
+
+    display_image.short_description = 'Изображение товара'
+    list_display_links = ('name',)
+    
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('name',)
+
+    
